678f634f60645db6386d9068a22159396ef58d4d
Daniel <danieldtran2013@gmail.com>
2023-11-02 02:17:37 -0400

    ejs addition

---
+++ ejs/server.js
@@ -0,0 +1,28 @@
+var express = require('express');
+var app = express();
+
+// set the view engine to ejs
+app.set('view engine', 'ejs');
+
+// djhome page
+app.get('/', function(req, res) {
+    res.render('djhome');
+  });
+
+// djprofiles page
+app.get('/djprofile', function(req, res) {
+    res.render('djprofile');
+  });
+
+// djsettings page
+app.get('/djsettings', function(req, res) {
+    res.render('djsettings');
+  });
+
+// topsongs page
+app.get('/topsongs', function(req, res) {
+    res.render('topsongs');
+  });
+
+app.listen(8080);
+console.log('Server is listening on port 8080');
